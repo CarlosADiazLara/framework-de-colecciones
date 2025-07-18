833c3f2d0d03d9502d46575e0cc65221b0f5c0b9
CarlosADiazLara <solrakyl@gmail.com>
2025-07-18 18:17:13 +0200

    antes de crear rama creando colecciones

--- framework-de-colecciones/src/main/java/com/example/App.java
+++ framework-de-colecciones/src/main/java/com/example/App.java
@@ -1,7 +1,10 @@
 package com.example;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class App {
     public static void main(String[] args) {
@@ -22,6 +25,53 @@ public class App {
     	// utilizar una de las clases que implementan los metodos de la coleccion
     	numerosEnteros = new ArrayList<>();
     	
+    	// Agregar elementos a la coleccion de numerosEnteros
+    	numerosEnteros.add(10);
+    	numerosEnteros.add(20);
     	
+    	// Declarar una coleccion de nombres
+    	List<String> nombres = new ArrayList<>();
+    	
+    	nombres.add("Carlos");
+    	nombres.add("Mauricio");
+    	
+    	/* Las colecciones anteriores permiten agregar a la coleccion cualquier cantidad de elementos.
+    	 * 
+    	 * Si queremos crear una coleccion que sea de tamaño fijo, lo podemos hacer a partir de un array*/
+    	
+    	String[] arrayNombres = {
+    			"Ramon", "Belepo", "Edwin", "Tahiel", "Ramon", "Belepo", "Edwin"
+    	};
+    	    	
+    	// Crear a partir del array anterior una coleccion que no permita elementos duplicados
+    	
+    	// VARIANTE #1 segun le ha sugerido la IA a Mauricio
+    	Set<String> nombresSinDuplicados1 = new HashSet<String>();
+    	
+    	for (String nombre : arrayNombres) {
+    		nombresSinDuplicados1.add(nombre);
+    	}
+    	
+    	System.out.println("Nombres No Repetidos, segun la variante #1");  	
+    	System.out.println(nombresSinDuplicados1); 
+    	
+    	// VARIANTE #2
+    	List<String> nombresConDuplicados = Arrays.asList(arrayNombres);
+    	
+    	Set<String> nombresSinDuplicados2 = new HashSet<String>();
+    	
+    	nombresSinDuplicados2.addAll(nombresConDuplicados);
+    	
+    	System.out.println("Nombres No Repetidos, segun la variante #2");  	
+    	System.out.println(nombresSinDuplicados2);
+    	
+    	// Desde la terminal:
+    	// git status --> indica si queda algo por hacer commit
+    	// git add . --> agregalo todo a la zona
+    	// git status --> de nuevo para comprobar
+    	// git log --> Aparecen los repositorios
+    	// git commit -m "antes de crear rama creando colecciones"
+    	
+    
     }
 }
